e9331505956f9a0b69a55ecac100a061ae0216a8
Shelby Lee <leegirlmiddles@gmail.com>
2026-04-21 09:22:57 -0500

    Add Terraform configuration for Azure resources

---
+++ .github/actions/terraform/stage1/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.68.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "rg-acmp-final"
+    storage_account_name = "acmp2400storageaccount"
+    container_name = "big-tf-state-acmp2400"
+    use_azuread_auth = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_container_registry" "student-acr" {
+  name = "acrshelbyleeacmp2400"
+  resource_group_name = "rg-shelbylee"
+  location = "Central US"
+  sku = "Basic"
+  admin_enabled = false
+}
