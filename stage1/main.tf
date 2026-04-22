814078a723104893d0ffbb86d8e3c9698a1c62a3
Shelby Lee <leegirlmiddles@gmail.com>
2026-04-21 19:30:02 -0500

    Update container name in Terraform configuration

--- stage1/main.tf
+++ stage1/main.tf
@@ -8,7 +8,7 @@ terraform {
   backend "azurerm" {
     resource_group_name = "rg-acmp-final"
     storage_account_name = "acmp2400storageaccount"
-    container_name = "big-tf-state-amcp2400"
+    container_name = "big-tf-state-acmp2400"
     use_azuread_auth = true
   }
 }
