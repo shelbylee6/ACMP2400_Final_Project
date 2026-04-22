de3ee11b78044d1a035cbbbe2c4fb21207d673e2
Shelby Lee <leegirlmiddles@gmail.com>
2026-04-21 19:27:57 -0500

    Disable terraform init in entrypoint.sh
    Comment out terraform initialization commands.

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -8,8 +8,8 @@ export ARM_TENANT_ID=${INPUT_ARM_TENANT_ID}
 export STATE_KEY=${INPUT_STATE_KEY}
 export TF_STAGE=${INPUT_TF_STAGE}
 
-cd /github/workspace/.github/actions/terraform/stage1
+#cd /github/workspace/.github/actions/terraform/stage1
 
-terraform init -backend-config="key=${INPUT_STATE_KEY}" -input=false -reconfigure
+#terraform init -backend-config="key=${INPUT_STATE_KEY}" -input=false -reconfigure
 terraform plan
 terraform apply --auto-approve
