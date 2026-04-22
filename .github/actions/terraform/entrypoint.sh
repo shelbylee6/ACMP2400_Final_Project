7a640a02c5e5e42c28fdcec633836a426c2e9832
Shelby Lee <leegirlmiddles@gmail.com>
2026-04-21 20:09:17 -0500

    Refactor Terraform commands in entrypoint.sh

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -8,10 +8,12 @@ export ARM_TENANT_ID=${INPUT_ARM_TENANT_ID}
 export STATE_KEY=${INPUT_STATE_KEY}
 export TF_STAGE=${INPUT_TF_STAGE}
 
-terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
-terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
-terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
+
 
 
 terraform plan
 terraform apply --auto-approve
+
+terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
+terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
+terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
