168bf0d8e7804a9f3843cb9b2f3be6c6df905d7c
Shelby Lee <leegirlmiddles@gmail.com>
2026-04-23 10:48:58 -0500

    Update entrypoint.sh

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 
 export ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}
 export ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}
@@ -7,11 +8,16 @@ export ARM_SUBSCRIPTION_ID=${INPUT_ARM_SUBSCRIPTION_ID}
 export ARM_TENANT_ID=${INPUT_ARM_TENANT_ID}
 export STATE_KEY=${INPUT_STATE_KEY}
 export TF_STAGE=${INPUT_TF_STAGE}
+export DJANGO_SECRET_KEY=${DJANGO_SECRET_KEY_PROD}
 
 
 
 
-
-terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
-terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
-terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
+if [[ "$TF_STAGE" = "stage1" ]]; then 
+  terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
+  terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
+  terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
+elif [[ "$TF_STAGE" == "stage2" ]]; then
+  terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
+  terraform -chdir=${INPUT_TF_STAGE} apply -auto-approve -var="ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}" -var="ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}" -var="DJANGO_SECRET_KEY=${INPUT_DJANGO_SECRET_KEY}"
+  fi
